1bb85b02d239c6a1284c13ac2727a9448271d8c9
Diane Doan <dianeddoan@gmail.com>
2026-04-06 05:26:14 -0600

    Change Gameboard and Player objects to  arrow functions

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 // Gameboard object
-const Gameboard = (function () {
-    let gameboard = [null, null, null, null, null, null, null, null, null]; // 3 x 3 square
+const Gameboard = (() => {
+    // 3 x 3 square
+    let gameboard = [
+        null, null, null,
+        null, null, null,
+        null, null, null
+    ]; 
 
-    return {
-        gameboard,
+    // let gameboard = [
+    //     'X', 'O', 'O',
+    //     'X', 'O', 'X',
+    //     null, 'X', 'O'
+    // ]; 
 
-        // place X/O on specified gameboard area
-        placeMove(letter, index) {
-            gameboard[index] = letter;
-        },
+    // place X/O on specified gameboard area
+    const placeMove = (letter, index) => {
+        gameboard[index] = letter;
+    }
 
-        // reset gameboard
-        resetGameboard() {
-            return gameboard.fill(null);
-        }
-    };
+    // reset gameboard
+    const resetGameboard = () => {
+        return gameboard.fill(null);
+    }
+
+    return { gameboard, placeMove, resetGameboard };
 })();
 
 // Player object
-function createPlayer(letter) {
+const Player = (letter) => {
     let score = 0;
 
-    return {
-        letter,
+    // increment win score
+    const playerWin = () => {
+        score++;
+        console.log(`Player ${letter} wins!`);
+    }
+    
+    // get current score
+    const getScore = () => {
+        return score;
+    }
 
-        // increment win score
-        playerWin() {
-            score++;
-            console.log(`Player ${letter} wins!`);
-        },
-
-        // get current score
-        getScore() {
-            return score;
-        }
-    };
-}
+    return { letter, playerWin, getScore };
+};
